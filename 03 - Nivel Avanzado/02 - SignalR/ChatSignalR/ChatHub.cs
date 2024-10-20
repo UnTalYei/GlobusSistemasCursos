7856a59a610e9fdd55c2ec55d3ba0a16f8594f42
Londoño Tabarez, Yeison <Ylondonotz@gmail.com>
2024-10-19 21:59:28 -0500

    Se suben archivos de ChatSignalR

---
+++ 03 - Nivel Avanzado/02 - SignalR/ChatSignalR/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho", $"Se ha conectado alguien {Context.ConnectionId}");
+        }
+    }
+}
